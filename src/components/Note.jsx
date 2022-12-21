ec69e629ff2d8b06c041568e462653cc3707d805
Mukul Thakur <mukulxt@gmail.com>
2022-12-21 23:37:00 +0530

    jsx

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>Keeper</h1>
+        <p>Google notes clone</p>
+    </div>
+}
+
+export default Note ;
